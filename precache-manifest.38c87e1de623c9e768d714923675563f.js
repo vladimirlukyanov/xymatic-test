5f007f200e48eeb7a8cbedb3fd50ce489177cee6
Vladimir Lukyanov <vladimir@liikyanov.com>
2020-09-17 10:22:12 +0300

    Updates

---
+++ precache-manifest.38c87e1de623c9e768d714923675563f.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a293a46c9feff78043041c569ec76ae",
+    "url": "/xymatic-test/index.html"
+  },
+  {
+    "revision": "ad9fb80aaa54c26f5a71",
+    "url": "/xymatic-test/static/css/main.b535bba2.chunk.css"
+  },
+  {
+    "revision": "485e78c63deea5e9daec",
+    "url": "/xymatic-test/static/js/2.29ec09c1.chunk.js"
+  },
+  {
+    "revision": "dce6a7c464b32723e62d58dd90942369",
+    "url": "/xymatic-test/static/js/2.29ec09c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9fb80aaa54c26f5a71",
+    "url": "/xymatic-test/static/js/main.b9839b46.chunk.js"
+  },
+  {
+    "revision": "285601e3928c5e4fc241",
+    "url": "/xymatic-test/static/js/runtime-main.7308d4d9.js"
+  },
+  {
+    "revision": "67e2b9d795fc068c08cef7e07ea5171b",
+    "url": "/xymatic-test/static/media/avatar-1.67e2b9d7.jpg"
+  },
+  {
+    "revision": "41a180f34b2da436150ac0442c88b9f2",
+    "url": "/xymatic-test/static/media/avatar-3.41a180f3.jpg"
+  },
+  {
+    "revision": "e0ab315a0e2e82eb9fec1387bae6600d",
+    "url": "/xymatic-test/static/media/avatar-4.e0ab315a.jpg"
+  },
+  {
+    "revision": "b5c826fc619edc4cc35993d0cd3c827c",
+    "url": "/xymatic-test/static/media/bg-ico-hero.b5c826fc.jpg"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/xymatic-test/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/xymatic-test/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "5e99edcb80abc73c7a06aacfddca51bf",
+    "url": "/xymatic-test/static/media/boxicons.5e99edcb.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/xymatic-test/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/xymatic-test/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "11fc83ae11617015f2fcde2065fb34d3",
+    "url": "/xymatic-test/static/media/dripicons-v2.11fc83ae.woff"
+  },
+  {
+    "revision": "7e12564e72400735ae5b671780f6a9f7",
+    "url": "/xymatic-test/static/media/dripicons-v2.7e12564e.eot"
+  },
+  {
+    "revision": "835984438066bd8944a7692e331152a7",
+    "url": "/xymatic-test/static/media/dripicons-v2.83598443.svg"
+  },
+  {
+    "revision": "cf09c981aeaa8736810133ab1148e4a3",
+    "url": "/xymatic-test/static/media/dripicons-v2.cf09c981.ttf"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/xymatic-test/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/xymatic-test/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/xymatic-test/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/xymatic-test/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/xymatic-test/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/xymatic-test/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/xymatic-test/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/xymatic-test/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/xymatic-test/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/xymatic-test/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/xymatic-test/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/xymatic-test/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/xymatic-test/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/xymatic-test/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/xymatic-test/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "3b232eb9958e6e06444c65e0643067c7",
+    "url": "/xymatic-test/static/media/img-1.3b232eb9.png"
+  },
+  {
+    "revision": "cfa5642716052b38f791566aa5437540",
+    "url": "/xymatic-test/static/media/logo-light-big.cfa56427.svg"
+  },
+  {
+    "revision": "505bc5e66b5fc3b4f5315e97a0a48784",
+    "url": "/xymatic-test/static/media/logo-light.505bc5e6.svg"
+  },
+  {
+    "revision": "80a62a17220e1c92bd8f9a5872ffe898",
+    "url": "/xymatic-test/static/media/logo.80a62a17.svg"
+  },
+  {
+    "revision": "63d2a5950fc212096c3612f71ae66f64",
+    "url": "/xymatic-test/static/media/materialdesignicons-webfont.63d2a595.woff"
+  },
+  {
+    "revision": "7fb0e3780372f10f804513e2e0c39e78",
+    "url": "/xymatic-test/static/media/materialdesignicons-webfont.7fb0e378.woff2"
+  },
+  {
+    "revision": "a1a0ed860f50ffd42ba46d7c10f30255",
+    "url": "/xymatic-test/static/media/materialdesignicons-webfont.a1a0ed86.eot"
+  },
+  {
+    "revision": "fe1545ef4dd1eef2f1e25528898fc0b3",
+    "url": "/xymatic-test/static/media/materialdesignicons-webfont.fe1545ef.ttf"
+  },
+  {
+    "revision": "ba4e037e1b9676c5215541748eaa1e42",
+    "url": "/xymatic-test/static/media/profile-img.ba4e037e.png"
+  }
+]);
